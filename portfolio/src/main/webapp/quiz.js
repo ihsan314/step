c2f5a1ce2180e3207b22060bb1f7d93f272c87bb
Ihsan Olawale <ihsanolawale@outlook.com>
2020-06-26 20:23:18 +0000

    Added Questions object and prepared logic for quiz feature

--- portfolio/src/main/webapp/quiz.js
+++ portfolio/src/main/webapp/quiz.js
@@ -1,6 +1,18 @@
+/**
+ * Question data type.
+ */
+function Question(question, options, correct) {
+  this.question = question;
+  this.options = options;
+  this.correct = correct; // index where correct answer is
+}
 function presentQuiz() {
-	// read array of quiz questions from a JSON file
-	//
-	// present questions in container, show correct answer after guess
-	// is supplied
+  // create array of quiz questions 
+  let questions;
+
+  for (q in questions) {
+    // display the question in a container
+    // Display the options
+    // When user answers display correct answer
+  }
 }
