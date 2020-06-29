928f9b0d8b4221f374e3ed7bc4f5d4d476f32b35
Ihsan Olawale <ihsanolawale@outlook.com>
2020-06-29 16:27:17 -0700

    Changed Question datatype to be a class and replaced let with const

--- portfolio/src/main/webapp/quiz.js
+++ portfolio/src/main/webapp/quiz.js
@@ -1,16 +1,18 @@
 /**
  * Question data type.
  */
-function Question(question, options, correct) {
-  this.question = question;
-  this.options = options;
-  this.correct = correct; // index where correct answer is
+class Question {
+  constructor(question, options, correct) {
+    this.question = question;
+    this.options = options;
+    this.correct = correct; // index where correct answer is
+  }
 }
 function presentQuiz() {
   // create array of quiz questions 
-  let questions;
+  const questions;
 
-  let quizContainer = document.getElementById('quiz-container');
+  const quizContainer = document.getElementById('quiz-container');
   for (q in questions) {
     // display the question in a container
     quizContainer.innerText = q.question;
