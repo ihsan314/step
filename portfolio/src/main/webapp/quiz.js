5b337fe71a0c3bc7b147e6839d6d180d3d13d2f4
Ihsan Olawale <ihsanolawale@outlook.com>
2020-06-30 01:38:27 +0000

    Added additional containers and privatised Question's members

--- portfolio/src/main/webapp/quiz.js
+++ portfolio/src/main/webapp/quiz.js
@@ -3,21 +3,24 @@
  */
 class Question {
   constructor(question, options, correct) {
-    this.question = question;
-    this.options = options;
-    this.correct = correct; // index where correct answer is
+    this._question = question;
+    this._answers = answers;
+    this._correctAnswerIndex = correctAnswerIndex;
   }
 }
+
 function presentQuiz() {
   // create array of quiz questions 
   const questions;
 
   const quizContainer = document.getElementById('quiz-container');
+  const questionContainer = document.getElementById('question-container');
+  const answersContainer = document.getElementById('answers-container');
+  const submitContainer = document.getElementById('submit-container');
+
   for (q in questions) {
     // display the question in a container
-    quizContainer.innerText = q.question;
     // Display the options
-    // Make another container
     // Use radio buttons
     // Have a submit button
     // When user clicks submit button display correct answer
