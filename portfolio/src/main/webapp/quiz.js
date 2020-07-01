30e72e664af650dab4f832f1442190707850a368
Ihsan Olawale <ihsanolawale@outlook.com>
2020-07-01 00:19:02 +0000

    Added preliminary questions

--- portfolio/src/main/webapp/quiz.js
+++ portfolio/src/main/webapp/quiz.js
@@ -23,7 +23,25 @@ class Question {
 
 function presentQuiz() {
   // create array of quiz questions 
-  const questions;
+  const questions = [];
+  questions.push(
+      new Question('What\'s my GitHub username?',
+		  ['iolawale', 'ihsan', 'olawale314', 'ihsan314'], 3));
+  questions.push(
+      new Question('Where do I reside?',
+		  ['San Francisco, CA', 'Vancouver, BC', 'Toronto, ON', 'Palo Alto CA'], 1));
+  questions.push(
+      new Question('Where do I attend university?',
+		  ['University of British Columbia', 'Simon Fraser University',
+		    'Stanford University', 'New York University'], 0));
+  questions.push(
+      new Question('In which of these classes did I receive the highest grade?',
+		  ['Principles of Microeconomics', 'Honours Integral Calculus',
+		    'Basic Algorithms and Data Structures', 'Introduction to Microcomputers'], 1));
+  questions.push(
+      new Question('In which of these classes did I receive the lowest grade?',
+		  ['Principles of Microeconomics', 'Honours Integral Calculus',
+		    'Basic Algorithms and Data Structures', 'Introduction to Microcomputers'], 3));
 
   const quizContainer = document.getElementById('quiz-container');
   const questionContainer = document.getElementById('question-container');
