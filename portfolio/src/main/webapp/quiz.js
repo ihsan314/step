9ee7d36420f0e8daeedcb3100505554199f66c20
Ihsan Olawale <ihsanolawale@outlook.com>
2020-07-06 18:23:45 -0700

    Used more descriptive variable names and added function to generate element id

--- portfolio/src/main/webapp/quiz.js
+++ portfolio/src/main/webapp/quiz.js
@@ -45,16 +45,16 @@ questions.push(
 function presentQuiz() {
   const questionsContainer = document.getElementById('questions-container');
   for (let questionNum = 0; questionNum < questions.length; questionNum++) {
-    const q = questions[questionNum];
-    const answers = q.getAnswers();
-    const correctIndex = q.getCorrectAnswerIndex();
+    const question = questions[questionNum];
+    const answers = question.getAnswers();
+    const correctIndex = question.getCorrectAnswerIndex();
     // Display the options
-    const questionStatement = document.createElement('p');
-    const question = document.createTextNode(`Question ${questionNum}: ${q.getQuestion()}`);
-    questionStatement.appendChild(question);
+    const questionStatementElement = document.createElement('p');
+    const questionStatementText = document.createTextNode(`Question ${questionNum}: ${q.getQuestion()}`);
+    questionStatementElement.appendChild(questionStatementText);
 
     const quizArea = document.createElement('select');
-    quizArea.setAttribute('id', `question-${questionNum}`);
+    quizArea.setAttribute('id', generateId(questionNum));
     for (let answerIndex = 0; answerIndex < answers.length; answerIndex++) {
       const option = document.createElement('option');
       option.setAttribute('value', answerIndex);
@@ -77,13 +77,16 @@ function checkSubmission() {
   const submitContainer = document.getElementById('submit-container');
   submitContainer.innerText = '';
   for (let questionNum = 0; questionNum < questions.length; questionNum++) {
-    const q = questions[questionNum];
-    const answers = q.getAnswers();
-    const correctIndex = q.getCorrectAnswerIndex();
-    if (document.getElementById('question-'+questionNum).selectedIndex !== correctIndex) {
+    const question = questions[questionNum];
+    const correctIndex = question.getCorrectAnswerIndex();
+    if (document.getElementById(generateId(questionNum)).selectedIndex !== correctIndex) {
       submitContainer.innerText += `Question ${questionNum} is incorrect\n`
     } else {
       submitContainer.innerText += `Question ${questionNum} is correct\n`
     }
   }
+}
+
+function generateId(questionNum) {
+  return `question-${questionNum}`;
 }
