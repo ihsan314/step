a1353f139906cb7d450b5de158253149af2ff020
Ihsan Olawale <ihsanolawale@outlook.com>
2020-07-13 12:18:36 -0700

    Marked messages as final and used ConcurrentHashMap

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -16,7 +16,7 @@ package com.google.sps.servlets;
 
 import com.google.gson.Gson;
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.ConcurrentHashMap;
 import java.util.Map;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,7 +26,7 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that returns some example content. */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-  private Map<String, String> messages = new HashMap<String, String>();
+  private final Map<String, String> messages = new ConcurrentHashMap<String, String>();
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
