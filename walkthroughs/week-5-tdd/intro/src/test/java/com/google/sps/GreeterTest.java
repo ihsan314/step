df65c901583a83bc1ce1f3948b3e30b4258af3b4
Ihsan Olawale <43079005+ihsan314@users.noreply.github.com>
2020-07-17 14:27:16 -0700

    Greeter coding and testing (#11)
    * Added whitespace trimming and test
    
    * Added functionality to trim symbols along with test
    
    * Renamed test appropriately and added new test
    
    * Tested what happens with embedded spaces
    
    * Adjusted regex to exclude the replacement of spaces

--- walkthroughs/week-5-tdd/intro/src/test/java/com/google/sps/GreeterTest.java
+++ walkthroughs/week-5-tdd/intro/src/test/java/com/google/sps/GreeterTest.java
@@ -30,4 +30,40 @@ public final class GreeterTest {
 
     Assert.assertEquals("Hello Ada", greeting);
   }
+
+  @Test
+  public void testGreetingTrimsWhitespace() {
+    Greeter greeter = new Greeter();
+
+    String greeting = greeter.greet("   Ada   ");
+
+    Assert.assertEquals("Hello Ada", greeting);
+  }
+
+  @Test
+  public void testGreetingTrimsSymbols() {
+    Greeter greeter = new Greeter();
+
+    String greeting = greeter.greet("@#$Ada%^&");
+
+    Assert.assertEquals("Hello Ada", greeting);
+  }
+
+  @Test
+  public void testGreetingTrimsSymbolsAndWhitespace() {
+    Greeter greeter = new Greeter();
+
+    String greeting = greeter.greet(" @#$Ada%^&  ");
+
+    Assert.assertEquals("Hello Ada", greeting);
+  }
+
+  @Test
+  public void testGreetingMaintainsEmbeddedSpaces() {
+    Greeter greeter = new Greeter();
+
+    String greeting = greeter.greet("  Ada Lovelace  ");
+
+    Assert.assertEquals("Hello Ada Lovelace", greeting);
+  }
 }
