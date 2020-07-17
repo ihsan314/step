26faee23c54b57710ff153a829231ccdf12dec87
Ihsan Olawale <ihsanolawale@outlook.com>
2020-07-17 11:01:07 -0700

    Added largely complete implementation for FindMeetingQuery

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,67 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Predicate;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    Collection<TimeRange> options = new ArrayList<>();
+    List<Event> eventsSorted = new ArrayList<>();
+    eventsSorted.addAll(events);
+    Collections.sort(
+        eventsSorted,
+        new Comparator<Event>() {
+          @Override
+          public int compare(Event a, Event b) {
+            return TimeRange.ORDER_BY_START.compare(a.getWhen(), b.getWhen());
+          }
+        });
+    eventsSorted.removeIf(
+        new Predicate<Event>() {
+          @Override
+          public boolean test(Event e) {
+            Collection<String> eventAttendees = new ArrayList<>();
+            eventAttendees.addAll(e.getAttendees());
+            eventAttendees.removeAll(request.getAttendees());
+            return e.getAttendees().size() == eventAttendees.size();
+          }
+        });
+
+    if (eventsSorted.isEmpty()) {
+      options.add(TimeRange.WHOLE_DAY);
+    } else {
+      Iterator<Event> eventsIter = eventsSorted.iterator();
+      Event event = eventsIter.next();
+      TimeRange option =
+          TimeRange.fromStartEnd(TimeRange.START_OF_DAY, event.getWhen().start(), false);
+      if (option.duration() >= request.getDuration()) {
+        options.add(option);
+      }
+      while (eventsIter.hasNext()) {
+        Event nextEvent = eventsIter.next();
+        if (!nextEvent.getWhen().overlaps(event.getWhen())) {
+          option =
+              TimeRange.fromStartEnd(event.getWhen().end(), nextEvent.getWhen().start(), false);
+          if (option.duration() >= request.getDuration()) {
+            options.add(option);
+          }
+        }
+        event = nextEvent;
+      }
+      option = TimeRange.fromStartEnd(event.getWhen().end(), TimeRange.END_OF_DAY, true);
+      if (option.duration() >= request.getDuration()) {
+        options.add(option);
+      }
+    }
+    if (TimeRange.WHOLE_DAY.duration() < request.getDuration()) {
+      options.clear();
+    }
+    return options;
   }
 }
