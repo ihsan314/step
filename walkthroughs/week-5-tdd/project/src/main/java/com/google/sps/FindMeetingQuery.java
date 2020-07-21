99cd4e370b4f287e20670d6f67350400c21be1a5
Ihsan Olawale <43079005+ihsan314@users.noreply.github.com>
2020-07-21 12:56:07 -0700

    Added largely complete implementation for FindMeetingQuery (#12)
    * Added largely complete implementation for FindMeetingQuery
    
    * Added new method to create new TimeRange and insert it in Collection
    
    * Iterated over Events by end time instead of start time
    
    * Refactored FindMeetingQuery further
    
    * Introduced new function findAvailableTimeSlots
    
    * Made all private member functions static
    
    * Change sortEvents to use streams and removed anonymous Comparator classes
    
    * Moved over 24 hour duration check to beginning of findAvailableTimeSlots
    
    * Removed anonymous Predicate class and used Collections.disjoint in clearIrrelevantEvents
    
    * Renamed clearIrrelevantEvents to clearUnattendedEvents
    
    * Formatted file with fix_java.sh

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,76 @@
 
 package com.google.sps;
 
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
+
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    List<Event> eventsSortedByEnd = sortEvents(events, TimeRange.ORDER_BY_END);
+
+    clearUnattendedEvents(eventsSortedByEnd, request.getAttendees());
+
+    return findAvailableTimeSlots(eventsSortedByEnd, request.getDuration());
+  }
+
+  private static Collection<TimeRange> findAvailableTimeSlots(
+      Collection<Event> eventsSortedByEnd, long minDuration) {
+    Collection<TimeRange> options = new ArrayList<>();
+
+    if (TimeRange.WHOLE_DAY.duration() < minDuration) {
+      return options;
+    }
+
+    if (eventsSortedByEnd.isEmpty()) {
+      options.add(TimeRange.WHOLE_DAY);
+      return options;
+    }
+
+    Event event = getFirstEvent(eventsSortedByEnd);
+    addTimeRangeIfPossible(options, TimeRange.START_OF_DAY, event.getWhen().start(), minDuration);
+
+    Iterator<Event> eventsIter = eventsSortedByEnd.iterator();
+    while (eventsIter.hasNext()) {
+      Event nextEvent = eventsIter.next();
+      if (!nextEvent.getWhen().overlaps(event.getWhen())) {
+        addTimeRangeIfPossible(
+            options, event.getWhen().end(), nextEvent.getWhen().start(), minDuration);
+      }
+      event = nextEvent;
+    }
+
+    addTimeRangeIfPossible(options, event.getWhen().end(), TimeRange.END_OF_DAY, minDuration);
+
+    return options;
+  }
+
+  private static void addTimeRangeIfPossible(
+      Collection<TimeRange> options, int beginTime, int endTime, long minDuration) {
+    TimeRange option = TimeRange.fromStartEnd(beginTime, endTime, endTime == TimeRange.END_OF_DAY);
+    if (option.duration() >= minDuration) {
+      options.add(option);
+    }
+  }
+
+  private static Event getFirstEvent(Collection<Event> events) {
+    List<Event> eventsSorted = sortEvents(events, TimeRange.ORDER_BY_START);
+    return eventsSorted.get(0);
+  }
+
+  private static List<Event> sortEvents(
+      Collection<Event> events, Comparator<TimeRange> sortingOrder) {
+    return events.stream().sorted(comparing(Event::getWhen, sortingOrder)).collect(toList());
+  }
+
+  private static void clearUnattendedEvents(
+      Collection<Event> events, Collection<String> meetingAttendees) {
+    events.removeIf(e -> Collections.disjoint(e.getAttendees(), meetingAttendees));
   }
 }
